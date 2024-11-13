a7c5fd942ec620887bbe306305382923ede0717b
Calvin Smith <email@cjsmith.io>
2024-11-13 15:09:00 -0700

    way more fields in the output

--- openhands.py
+++ openhands.py
@@ -1,12 +1,9 @@
 from datetime import datetime
-import json
 import pandas as pd
 
 import click
 import zeno_client
 
-
-from models import SWEBenchInstance
 from models.openhands import BenchmarkPath
 
 
@@ -19,20 +16,16 @@ def load_dataset(benchmark_path: BenchmarkPath) -> pd.DataFrame:
     """
     rows: list[pd.DataFrame] = []
 
-    with open(benchmark_path.trajectories, "r") as file:
-        for line in file.readlines():
-            data = json.loads(line)
-            row = pd.DataFrame(
-                [
-                    {
-                        "id": data.get("instance_id"),
-                        "problem_statement": data.get("instance", {}).get(
-                            "problem_statement"
-                        ),
-                    }
-                ]
-            )
-            rows.append(row)
+    for trajectory in benchmark_path.load_trajectories():
+        row = pd.DataFrame(
+            [
+                {
+                    "id": trajectory.instance_id,
+                    "problem_statement": trajectory.instance.problem_statement,
+                }
+            ]
+        )
+        rows.append(row)
 
     dataset = pd.concat(rows, ignore_index=True)
     dataset["statement_length"] = dataset["problem_statement"].apply(len)
@@ -42,40 +35,43 @@ def load_dataset(benchmark_path: BenchmarkPath) -> pd.DataFrame:
 
 
 def load_system(benchmark_path: BenchmarkPath) -> pd.DataFrame:
-    # Load the results. When we load a trajectory we'll find the associated instance object and
-    # flatten the needed parameters.
-    results: dict[str, SWEBenchInstance] = {}
-    with open(benchmark_path.results, "r") as file:
-        for line in file.readlines():
-            instance = SWEBenchInstance.model_validate_json(line)
-            results[instance.instance_id] = instance
+    """
+    Load a system (the agents performance over each instance in the dataset) from a benchmark.
+    """
+    # Load the results and metadata. When we convert a trajectory we'll flatten some of the
+    # relevant info stored in these objects.
+    results = benchmark_path.load_results()
+    metadata = benchmark_path.load_metadata()
 
-    # Load the trajectories.
-    trajectories: list[pd.DataFrame] = []
-    with open(benchmark_path.trajectories, "r") as file:
-        for line in file.readlines():
-            data = json.loads(line)
-            instance = results[data["instance_id"]]
-            trajectory = pd.DataFrame(
-                [
-                    {
-                        "id": data["instance_id"],
-                        "history": data["history"],
-                        "resolved": instance.test_result.report.resolved,
-                        "git_patch": instance.test_result.git_patch,
-                    }
-                ]
-            )
-            trajectories.append(trajectory)
+    rows: list[pd.DataFrame] = []
+    for trajectory in benchmark_path.load_trajectories():
+        test_result = results[trajectory.instance_id].test_result
 
-    return pd.concat(trajectories, ignore_index=True)
+        # Convert the trajectory to a dataframe.
+        row = pd.DataFrame(
+            [
+                {
+                    "id": trajectory.instance_id,
+                    "history": trajectory.history,
+                    # The test results contain the outputs and Boolean summaries of agent
+                    # performance.
+                    **test_result.dump(),
+                    # The metadata contains information about agent/LLM configuration. Should be
+                    # consistent across all trajectories in this system.
+                    **metadata.dump(),
+                }
+            ]
+        )
+        rows.append(row)
+
+    return pd.concat(rows, ignore_index=True)
 
 
 @click.command()
 @click.argument("benchmarks", type=click.Path(exists=True, file_okay=False), nargs=-1)
 @click.option("--project-title", type=str)
 @click.option("--zeno-api-key", type=str, envvar="ZENO_API_KEY")
-def cli(benchmarks, project_title, zeno_api_key):
+def cli(benchmarks, project_title, zeno_api_key) -> None:
     """
     Generate a ZenoML performance report for the given OpenHands benchmarks.
     """
